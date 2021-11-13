129e6e6cb40754d645e60608e415c57fb46b800b
Maikel Ramos <mkramost@gmail.com>
2021-11-13 15:36:02 -0500

    Conforming to MVC Pattern

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    var quizMain = QuizMain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+
+    @IBAction func verifyAnswer(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let checkUser = quizMain.checkAnswer(userAnswer)
+        
+        if checkUser{
+            sender.backgroundColor = UIColor.green
+        }else{
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizMain.questionProgress()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+    @objc func updateUI(){
+        questionLabel.text = quizMain.getQuestionText()
+        progressBar.progress = quizMain.getProgress()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        
+    }
+}
+
