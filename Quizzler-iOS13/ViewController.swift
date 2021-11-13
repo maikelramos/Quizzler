637dbe8dc9f4b960a131b012aab23c1090235ac3
Maikel Ramos <mkramost@gmail.com>
2021-11-13 14:49:32 -0500

    progress bar, multiple questions, user feedback

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -40,19 +40,24 @@ class ViewController: UIViewController {
         let userAnswer = sender.currentTitle
         let actualAnswer = quiz[questionNumber].answer
         if userAnswer == actualAnswer{
-            print("Right")
+            sender.backgroundColor = UIColor.green
+            print("exec green")
         }else{
-            print("Wrong")
+            sender.backgroundColor = UIColor.red
+            print("exec red")
         }
         if questionNumber+1 < quiz.count{
             questionNumber+=1
         }else{
             questionNumber=0
         }
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
-    func updateUI(){
+    @objc func updateUI(){
         questionLabel.text = quiz[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber+1)/Float(quiz.count)
     }
 }
 
