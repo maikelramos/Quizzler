4fbfeb53e562b2e9884c4b393a86ee296b1561e6
Maikel Ramos <mkramost@gmail.com>
2021-11-13 13:45:00 -0500

    button integration, answer check, update ui

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    var questionNumber = 0
+    var quiz = [["One","True"],["Two", "False"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func verifyAnswer(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        if userAnswer == actualAnswer{
+            print("Right")
+        }else{
+            print("Wrong")
+        }
+        if questionNumber+1 < quiz.count{
+            questionNumber+=1
+        }else{
+            questionNumber=0
+        }
+        updateUI()
+    }
+    func updateUI(){
+        questionLabel.text = quiz[questionNumber][0]
+    }
 }
 
